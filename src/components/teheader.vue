56cd29280d2d28da4bf8b131ab4d019d93d54c26
hank chou <a29554483@gmail.com>
2018-04-08 18:08:10 +0800

    [vue] 1. modify hover

--- src/components/teheader.vue
+++ src/components/teheader.vue
@@ -1,15 +1,15 @@
 <template>
-	<div class="fixedheader width-100 zindex-100 height-120 text-align-center ">
-        <div class="width-80 margin-auto box-flex text-align-center flex-direction-row flex-wrap flex-justify-center flex-items-center height-100">
-	        <div class="flex-1 height-30 text-align-left flex-self-center ">
+	<div class="fixedheader">
+        <div>
+	        <div>
 	            <span class="ion-navicon-round font-size-26" @click="openBottomSheet"></span>
 	        </div>
-	        <div class="flex-3 height-30 height-50 line-height-50 flex-direction-row flex-items-flex-start unselect">
+	        <div >
 	            <div class="flex-2"><img class="height-60" src="../assets/logo_b.png" lazy="loaded"></div>
 	        </div>
-	        <div class="flex-1 height-30 text-align-right flex-self-center">
+	        <div >
 	          <router-link to="/surch">
-	          <span class="ion-search font-size-26"></span>
+	          <span></span>
 	          </router-link>
 	        </div>
         </div>
